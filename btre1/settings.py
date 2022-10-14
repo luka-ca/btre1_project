018fa0eaf0c6d9632fd706204119723384576de4
Luka Čadež <luka.cadez@outlook.com>
2022-10-14 14:51:05 +0200

    message reda radi

--- btre1/settings.py
+++ btre1/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -128,8 +129,18 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre1/static')
 ]
 
+#Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# ovo je dodano u odnosu na Jackov video...valjda jer koristim linux kao op
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
